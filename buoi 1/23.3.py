47896121f7a1b7781704e68819271a8c104b34c9
TrongHuu <nguyen040107@gmail.com>
2026-04-19 20:58:34 +0700

    Revise insights on customer density and business strategies
    Updated analysis and recommendations for high and low density areas, emphasizing business strategies.

--- buoi 1/23.3.py
+++ buoi 1/23.3.py
@@ -12,3 +12,19 @@ for clat, clon, n in clusters:
 m = folium.Map(location=[center_lat, center_lon], zoom_start=13)
 HeatMap(data, radius=15, blur=10, max_zoom=17).add_to(m)
 m.save("bai_23_3_heatmap.html")
+
+
+
+"""
+* Các khu vực có mật độ cao cho thấy nơi tập trung nhiều khách hàng hoặc giao dịch, đây là những khu vực có nhu cầu lớn và tiềm năng kinh doanh cao.
+👉 Doanh nghiệp có thể:
+-Tăng cường đầu tư
+-Mở thêm chi nhánh
+-Đẩy mạnh marketing
+* Các vùng mật độ thấp có thể là:Thị trường chưa khai thác hoặc nhu cầu thấp
+👉 Doanh nghiệp có thể:
+-Nghiên cứu mở rộng
+-Hoặc tối ưu chi phí.
+🎯 Kết luận
+Bản đồ nhiệt giúp hỗ trợ ra quyết định trong việc phân bổ nguồn lực và định hướng chiến lược kinh doanh theo không gian.
+"""
